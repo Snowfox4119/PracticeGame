968c5dc6862c785deb348264246c73fb34dfd7dd
Snowfox4119 <gatobueno4119@gmail.com>
2024-11-01 12:31:38 -0400

    added basic boxes

--- main.cpp
+++ main.cpp
@@ -1,22 +1,44 @@
 #include <iostream>
 #include <raylib.h>
+#include <vector>
 
 using namespace std;
 
+float boardCord[4][4] = {
+    {100, 550, 100, 550},
+    {100, 100, 550, 550},
+    {350, 350, 350, 350},
+    {350, 350, 350, 350}
+};
+
+
+class Game {
+public:    
+
+    void Draw() {
+        int x = 0;
+        while (x < 4) {
+            DrawRectangleLinesEx(Rectangle{boardCord[0][x], boardCord[1][x], boardCord[2][x], boardCord[3][x]}, 2, WHITE);
+            x += 1;
+        }
+    }
+};
+
 int main () {
 
-    const int screenWidth = 800;
-    const int screenHeight = 600;
-
-    cout << "Hello World" << endl;
-
-    InitWindow(screenWidth, screenHeight, "Practice Game");
+    InitWindow(1000, 1000, "Practice Game");
     SetTargetFPS(60);
+
+    Game game = Game();
 
     while (WindowShouldClose() == false){
         BeginDrawing();
-        ClearBackground(WHITE);
-        DrawText("Text", 100, 100, 20, LIGHTGRAY);
+        ClearBackground(BLACK);
+        //game.Draw();
+        
+        game.Draw();
+
+        DrawText("Score: 0", 100, 50, 20, LIGHTGRAY);
         EndDrawing();
     }
 
